2b72e0724b2002a53b1de93d8b7c062465e617d6
Moddwyn <nathaliaandaya@gmail.com>
2023-05-07 11:47:32 -0700

    math game revise

--- Tom And Tony Project/Assets/Scripts/MathGame.cs
+++ Tom And Tony Project/Assets/Scripts/MathGame.cs
@@ -1,9 +1,7 @@
-using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using NaughtyAttributes;
 using TMPro;
-using UnityEditor;
 using UnityEngine;
 
 public class MathGame : MonoBehaviour
@@ -26,10 +24,10 @@ public class MathGame : MonoBehaviour
     public AudioClip correctAudio;
     public AudioClip wrongAudio;
 
-    
+
     [ReadOnly][SerializeField] bool startTimer;
     [ReadOnly][SerializeField] int correctButton;
-    
+
     void Start()
     {
         score = 0;
@@ -37,16 +35,62 @@ public class MathGame : MonoBehaviour
         startTimer = false;
         gameOverScreen.SetActive(false);
 
-        if(!PlayerPrefs.HasKey("Math_High"))
+        if (!PlayerPrefs.HasKey("Math_High"))
             PlayerPrefs.SetInt("Math_High", 0);
 
         highScore = PlayerPrefs.GetInt("Math_High");
+
+        PrintSimilarNumbers(238);
     }
 
     public void StartGame()
     {
         startTimer = true;
     }
+
+    int[] PrintSimilarNumbers(int num)
+    {
+        // Get the last digit of the input number
+        int lastDigit = num % 10;
+
+        // Generate two random numbers with the same last digit and decimal place as the input number
+        int similarNum1;
+        int similarNum2;
+        int rangeX = (int)System.Math.Pow(10, num.ToString().Length - 2) * 10;
+        int rangeY = (int)System.Math.Pow(10, num.ToString().Length - 1) * 10;
+
+        if (num.ToString().Length == 1)
+        {
+            similarNum1 = Random.Range(0, 10);
+            similarNum2 = Random.Range(0, 10);
+            while(similarNum1 == similarNum2 || similarNum1 == num || similarNum2 == num)
+            {
+                similarNum1 = Random.Range(0, 10);
+                similarNum2 = Random.Range(0, 10);
+            }
+        }
+        else
+        {
+            similarNum1 = Random.Range(rangeX, rangeY);
+            similarNum2 = Random.Range(rangeX, rangeY);
+
+            similarNum1 = (similarNum1 - (similarNum1 % 10)) + lastDigit;
+            similarNum2 = (similarNum2 - (similarNum2 % 10)) + lastDigit;
+            while (similarNum1 == similarNum2 || similarNum1 == num || similarNum2 == num)
+            {
+                similarNum1 = Random.Range(rangeX, rangeY);
+                similarNum2 = Random.Range(rangeX, rangeY);
+
+                similarNum1 = (similarNum1 - (similarNum1 % 10)) + lastDigit;
+                similarNum2 = (similarNum2 - (similarNum2 % 10)) + lastDigit;
+            }
+        }
+
+
+
+        return new int[] { similarNum1, similarNum2 };
+    }
+
 
     void Update()
     {
@@ -55,15 +99,15 @@ public class MathGame : MonoBehaviour
         finalScoreText.text = "Final Score: " + score;
         highScoreText.text = "High Score: " + highScore;
 
-        if(score >= highScore)
+        if (score >= highScore)
             highScore = score;
-        
-        if(timer > 0 && startTimer)
+
+        if (timer > 0 && startTimer)
         {
             timer -= Time.deltaTime;
         }
 
-        if(timer <= 0)
+        if (timer <= 0)
         {
             timer = 0;
             startTimer = false;
@@ -75,73 +119,71 @@ public class MathGame : MonoBehaviour
 
     void EquationGeneration()
     {
-        if(string.IsNullOrEmpty(equation) && startTimer)
+        if (string.IsNullOrEmpty(equation) && startTimer)
         {
             equation += Random.Range(1, 51) + "";
-            
+
             int randOperation = Random.Range(1, 4);
-            if(randOperation == 1)
+            if (randOperation == 1)
                 equation += " + ";
-            else if(randOperation == 1)
+            else if (randOperation == 1)
                 equation += " - ";
             else
                 equation += " * ";
 
             equation += Random.Range(1, 51) + "";
-            
-            int randButton = Random.Range(1,4);
+
+            int randButton = Random.Range(1, 4);
             List<double> numbers = new List<double>();
-            
+
             numbers.Add(Evaluate(equation));
-            while (numbers.Count < 3)
-            {
-                int number = Random.Range(1, 51);
-                if (!numbers.Contains(number))
-                {
-                    numbers.Add(number);
-                }
-            }
+            int[] otherChoices = PrintSimilarNumbers((int)numbers[0]);
+            numbers.Add(otherChoices[0]);
+            numbers.Add(otherChoices[1]);
             foreach (var item in numbers)
             {
                 print(item);
             }
-            if(randButton == 1)
+            if (randButton == 1)
             {
-                button1.text = numbers[0] +"";
-                button2.text = numbers[1] +"";
-                button3.text = numbers[2] +"";
+                button1.text = numbers[0] + "";
+                button2.text = numbers[1] + "";
+                button3.text = numbers[2] + "";
                 correctButton = 1;
-            } else if(randButton == 2)
+            }
+            else if (randButton == 2)
             {
-                button1.text = numbers[1] +"";
-                button2.text = numbers[0] +"";
-                button3.text = numbers[2] +"";
+                button1.text = numbers[1] + "";
+                button2.text = numbers[0] + "";
+                button3.text = numbers[2] + "";
                 correctButton = 2;
-            } else
+            }
+            else
             {
-                button1.text = numbers[2] +"";
-                button2.text = numbers[1] +"";
-                button3.text = numbers[0] +"";
+                button1.text = numbers[2] + "";
+                button2.text = numbers[1] + "";
+                button3.text = numbers[0] + "";
                 correctButton = 3;
             }
 
             equationText.text = equation + " = ?";
 
-            
+
         }
     }
 
     public void ChooseButton(int num)
     {
-        if(num == correctButton)
+        if (num == correctButton)
         {
             score++;
-            timer+= 5;
+            timer += 5;
             equation = "";
             GameManager.Instance.audioSource.PlayOneShot(correctAudio);
-        } else
+        }
+        else
         {
-            timer-= 5;
+            timer -= 5;
             GameManager.Instance.audioSource.PlayOneShot(wrongAudio);
         }
     }
