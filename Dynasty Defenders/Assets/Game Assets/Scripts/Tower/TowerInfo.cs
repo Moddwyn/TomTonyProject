1c713b762ebb84677cd1045caec92826e6f9416b
Moddwyn <nathaliaandaya@gmail.com>
2024-01-27 10:07:21 -0800

    fsdfsf

---
+++ Dynasty Defenders/Assets/Game Assets/Scripts/Tower/TowerInfo.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+
+[CreateAssetMenu()]
+public class TowerInfo : ScriptableObject
+{
+    public string towerName;
+    [Multiline] public string description;
+    public int cost;
+    [ShowAssetPreview] public Sprite icon;
+
+    [HorizontalLine]
+    public List<TowerUpgrade> upgrades = new List<TowerUpgrade>();
+}
+
+[System.Serializable]
+public class TowerUpgrade
+{
+    public string upgradeName;
+    public int cost;
+    [Multiline] public string description;
+}
